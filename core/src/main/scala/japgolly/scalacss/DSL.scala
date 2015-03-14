2b3b0ad717decf8abe78f2e73d545d5fdecc9db8
David Barri <japgolly@gmail.com>
2015-03-14 12:02:49 +1100

    Added DSL for units.

--- core/src/main/scala/japgolly/scalacss/DSL.scala
+++ core/src/main/scala/japgolly/scalacss/DSL.scala
@@ -1,11 +1,83 @@
 package japgolly.scalacss
 
+import shapeless.Witness
 import Style.{UnsafeExt, UnsafeExts}
 
 object DSL {
 
+  val zeroType = Witness(0)
+
   // -------------------------------------------------------------------------------------------------------------------
   // Implicits
+
+  @inline implicit def ValueZero(z: zeroType.T): String = "0"
+
+  @inline implicit final class DslInt(val self: Int) extends AnyVal {
+    @inline private def unit(u: String): Value = self.toString + u
+
+    /** Centimeters. */
+    @inline def cm = unit("cm")
+
+    /**
+     * This unit represents the width, or more precisely the advance measure, of the glyph '0' (zero, the Unicode
+     * character U+0030) in the element's font.
+     */
+    @inline def ch = unit("ch")
+
+    /**
+     * This unit represents the calculated font-size of the element.
+     * If used on the `font-size` property itself, it represents the inherited `font-size` of the element.
+     */
+    @inline def em = unit("em")
+
+    /**
+     * This unit represents the x-height of the element's font.
+     * On fonts with the 'x' letter, this is generally the height of lowercase letters in the font;
+     * 1ex ≈ 0.5em in many fonts.
+     */
+    @inline def ex = unit("ex")
+
+    /** Inches (1in = 2.54 cm). */
+    @inline def inches = unit("inches")
+
+    /** Millimeters. */
+    @inline def mm = unit("mm")
+
+    /** Picas (1pc = 12pt). */
+    @inline def pc = unit("pc")
+
+    /** Points (1pt = 1/72 of 1in). */
+    @inline def pt = unit("pt")
+
+    /**
+     * Pixel. Relative to the viewing device.
+     * For screen display, typically one device pixel (dot) of the display.
+     * For printers and very high resolution screens one CSS pixel implies multiple device pixels, so that the number
+     * of pixel per inch stays around 96.
+     */
+    @inline def px = unit("px")
+
+    /**
+     * This unit represents the `font-size` of the root element (e.g. the `font-size` of the `&lt;html&gt;` element).
+     * When used on the `font-size` on this root element, it represents its initial value.
+     */
+    @inline def rem = unit("rem")
+
+    /** 1/100th of the height of the viewport. */
+    @inline def vh = unit("vh")
+
+    /** 1/100th of the minimum value between the height and the width of the viewport. */
+    @inline def vmin = unit("vmin")
+
+    /** 1/100th of the maximum value between the height and the width of the viewport. */
+    @inline def vmax = unit("vmax")
+
+    /** 1/100th of the width of the viewport. */
+    @inline def vw = unit("vw")
+
+    /** Size as a percentage. */
+    @inline def pct = unit("%")
+  }
 
   @inline implicit final class DslAttr(val self: Attr) extends AnyVal {
     @inline def ~(value: Value): AV = AV(self, value)
--- core/src/test/scala/japgolly/scalacss/UsageTest.scala
+++ core/src/test/scala/japgolly/scalacss/UsageTest.scala
@@ -1,5 +1,6 @@
 package japgolly.scalacss
 
+import shapeless.test.illTyped
 import utest._
 import TestUtil._
 import Attrs._
@@ -11,13 +12,13 @@ object UsageTest extends TestSuite {
 
   val s1 =
     style(
-      margin ~ "12px",
-      padding ~ "2ex",
+      margin ~ 12.px,
+      padding ~ 2.ex,
 
       Hover(
         fontWeight ~ "normal",
-        lineHeight ~ "1em",
-        padding    ~ "0"),
+        lineHeight ~ 1.em,
+        padding    ~ 0),
 
       Visited.not(FirstChild)(
         fontWeight ~ "bold"),
@@ -27,37 +28,44 @@ object UsageTest extends TestSuite {
         color ~ "#000",
 
         unsafeChild("h1")(
-          fontSize ~ "150%"))
+          fontSize ~ 150.pct))
     )
 
   override val tests = TestSuite {
-    val css = StringRenderer defaultPretty Css.style(".a1", s1)(Env.empty)
-    // println(css)
-    assertEq(css.trim,
-      """
-        |.a1 {
-        |  margin: 12px;
-        |  padding: 2ex;
-        |}
-        |
-        |.a1:hover {
-        |  font-weight: normal;
-        |  line-height: 1em;
-        |  padding: 0;
-        |}
-        |
-        |.a1:not(:first-child):visited {
-        |  font-weight: bold;
-        |}
-        |
-        |.a1 nav.debug {
-        |  background-color: #f88;
-        |  color: #000;
-        |}
-        |
-        |.a1 nav.debug h1 {
-        |  font-size: 150%;
-        |}
-      """.stripMargin.trim)
+    'maintest {
+      val css = StringRenderer defaultPretty Css.style(".a1", s1)(Env.empty)
+      // println(css)
+      assertEq(css.trim,
+        """
+          |.a1 {
+          |  margin: 12px;
+          |  padding: 2ex;
+          |}
+          |
+          |.a1:hover {
+          |  font-weight: normal;
+          |  line-height: 1em;
+          |  padding: 0;
+          |}
+          |
+          |.a1:not(:first-child):visited {
+          |  font-weight: bold;
+          |}
+          |
+          |.a1 nav.debug {
+          |  background-color: #f88;
+          |  color: #000;
+          |}
+          |
+          |.a1 nav.debug h1 {
+          |  font-size: 150%;
+          |}
+        """.stripMargin.trim)
+    }
+
+    'zeroLiteral {
+      val ok: AV = padding ~ 0
+      illTyped("padding ~ 1")
+    }
   }
 }
