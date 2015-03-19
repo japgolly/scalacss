328e430c7fda80b39b8a429d97035cccb0140f12
David Barri <japgolly@gmail.com>
2015-03-19 16:54:18 +1100

    Script now merges from master too

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -4,7 +4,8 @@ cd "$(dirname "$(readlink -e "$0")")" || exit 1
 #[ $# -ne 1 ] && echo "Usage: $0 <xxx>" && exit 1
 #tmp=/tmp/$(date +%Y%m%d-%H%M%S)-$$
 
-bin/gitbook build doc book \
+git merge master \
+  && bin/gitbook build doc book \
   && git add -A book \
   && git st \
   && echo "git commit -m 'Updated book.'"
