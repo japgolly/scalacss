6e1738a8bb3dcc28057b6a75aa5446825ae81df1
David Barri <japgolly@gmail.com>
2015-03-22 22:52:11 +1100

    upd

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -4,7 +4,8 @@ cd "$(dirname "$(readlink -e "$0")")" || exit 1
 #[ $# -ne 1 ] && echo "Usage: $0 <xxx>" && exit 1
 #tmp=/tmp/$(date +%Y%m%d-%H%M%S)-$$
 
-git merge master \
+git pull \
+  && git merge master \
   && bin/gitbook build doc book \
   && git add -A book \
   && git st \
