27cab58b5a3be3fe6cb406654204bc1a8aebd81a
David Barri <japgolly@gmail.com>
2015-03-27 19:46:47 +1100

    Prepare next version

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,7 @@ object ScalaCSS extends Build {
     CDS.all(
       _.settings(
         organization       := "com.github.japgolly.scalacss",
-        version            := "0.1.0",
+        version            := "0.2.0-SNAPSHOT",
         homepage           := Some(url("https://github.com/japgolly/scalacss")),
         licenses           += ("LGPL v2.1+" -> url("http://www.gnu.org/licenses/lgpl-2.1.txt")),
         scalaVersion       := Scala211,
