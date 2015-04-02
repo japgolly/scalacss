8f66e8b6814bfbe61308cd3a474b18ddcb28ea8b
David Barri <japgolly@gmail.com>
2015-04-03 08:51:41 +1100

    Added {Dev,Prod}Defaults

--- core/src/main/scala/japgolly/scalacss/Defaults.scala
+++ core/src/main/scala/japgolly/scalacss/Defaults.scala
@@ -4,47 +4,7 @@ import scala.annotation.elidable
 import mutable._
 import Register.{ErrorHandler, NameGen}
 
-/**
- * Default config.
- *
- * Use the scalac `-Xelide-below` flag to switch from development- to production-mode.
- */
-trait Defaults extends PlatformDefaults {
-
-  @elidable(elidable.ASSERTION)
-  private def _devMode = true
-
-  protected def devMode = _devMode
-
-  val registerNameGen: NameGen =
-    if (devMode)
-      NameGen.numbered()
-    else
-      NameGen.short()
-
-  val registerErrorHandler: ErrorHandler =
-    if (devMode)
-      ErrorHandler.noisy
-    else
-      ErrorHandler.silent
-
-  implicit def register: Register =
-    new Register(registerNameGen, registerErrorHandler)
-
-  implicit def stringRenderer: Renderer[String] =
-    if (devMode)
-      StringRenderer.defaultPretty
-    else
-      StringRenderer.formatTiny
-
-  implicit def composition: Compose =
-    if (devMode)
-      Compose.safe
-    else
-      Compose.trust
-
-
-  // ↓ These aren't defaults... hmmm...
+trait DefaultImports {
 
   object StyleSheet {
     type Inline     = mutable.StyleSheet.Inline
@@ -54,4 +14,59 @@ trait Defaults extends PlatformDefaults {
   final type StyleA = japgolly.scalacss.StyleA
 }
 
+trait IDefaults extends PlatformDefaults with DefaultImports {
+           val registerNameGen     : NameGen
+           val registerErrorHandler: ErrorHandler
+  implicit def stringRenderer      : Renderer[String]
+  implicit def composition         : Compose
+
+  implicit def register: Register =
+    new Register(registerNameGen, registerErrorHandler)
+}
+
+/**
+ * Development-mode defaults. Pretty names, pretty CSS, warnings.
+ */
+object DevDefaults extends DevDefaults
+trait DevDefaults extends IDefaults {
+  override          val registerNameGen     : NameGen          = NameGen.numbered()
+  override          val registerErrorHandler: ErrorHandler     = ErrorHandler.noisy
+  override implicit def stringRenderer      : Renderer[String] = StringRenderer.defaultPretty
+  override implicit def composition         : Compose          = Compose.safe
+}
+
+/**
+ * Production-mode defaults. Minimised CSS, no warnings.
+ */
+object ProdDefaults extends ProdDefaults
+trait ProdDefaults extends IDefaults {
+  override          val registerNameGen     : NameGen          = NameGen.short()
+  override          val registerErrorHandler: ErrorHandler     = ErrorHandler.silent
+  override implicit def stringRenderer      : Renderer[String] = StringRenderer.formatTiny
+  override implicit def composition         : Compose          = Compose.trust
+}
+
+/**
+ * Default config.
+ *
+ * Use the scalac `-Xelide-below` flag to switch from development- to production-mode.
+ */
 object Defaults extends Defaults
+trait Defaults extends IDefaults {
+
+  @elidable(elidable.ASSERTION)
+  private def _devMode = true
+
+  protected def devMode = _devMode
+
+  protected def defaults: IDefaults =
+    if (devMode)
+      DevDefaults
+    else
+      ProdDefaults
+
+  override          val registerNameGen     : NameGen          = defaults.registerNameGen
+  override          val registerErrorHandler: ErrorHandler     = defaults.registerErrorHandler
+  override implicit def stringRenderer      : Renderer[String] = defaults.stringRenderer
+  override implicit def composition         : Compose          = defaults.composition
+}
