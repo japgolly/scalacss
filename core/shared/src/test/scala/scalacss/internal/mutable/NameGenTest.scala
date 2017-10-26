44e2fb92f2819b4c2e7eaa82af3fb4331b74bee8
Andreas Gabor <andreas@teichrib.de>
2017-10-26 13:43:43 +0200

    fixed classname generation for more than 26 registered inline stylesheets updated sbt (0.13.13 -> 0.13.16)

---
+++ core/shared/src/test/scala/scalacss/internal/mutable/NameGenTest.scala
@@ -0,0 +1,26 @@
+package scalacss.internal.mutable
+
+import utest._
+
+import scalacss.internal.ClassNameHint
+import scalacss.internal.mutable.Register.NameGen
+import scalacss.test.TestUtil._
+
+object NameGenTest extends TestSuite {
+
+  override val tests = TestSuite {
+    'nameGenUniqueAndAscii {
+      println((1 to 1000).map(_ => NameGen.short().next(ClassNameHint("placeholder"))._1.value))
+
+      val classesAmount = 1000
+      val classNames = (1 to classesAmount).map(_ => NameGen.short().next(ClassNameHint("placeholder"))._1.value)
+      val uniqueClassNames = classNames.toSet
+
+
+      assertEq(uniqueClassNames.size, classesAmount)
+      assertEq(uniqueClassNames.count(_.matches("^[a-z0-9_]+$")), classesAmount)
+
+      NameGen.resetNextShortPrefix()
+    }
+  }
+}
