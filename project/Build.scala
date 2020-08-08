280ceda73dfbef8414e82700f8471e58c842855c
Scala Steward <me@scala-steward.org>
2020-08-08 20:24:43 +0200

    Update scalajs-dom to 1.1.0

--- project/Build.scala
+++ project/Build.scala
@@ -27,7 +27,7 @@ object ScalaCssBuild {
     val Scala212        = "2.12.11"
     val Scala213        = "2.13.2"
     val ScalaCollCompat = "2.1.6"
-    val ScalaJsDom      = "1.0.0"
+    val ScalaJsDom      = "1.1.0"
     val ScalaJsReact    = "1.7.0"
     val Scalatags       = "0.9.1"
     val Scalaz          = "7.2.30"
