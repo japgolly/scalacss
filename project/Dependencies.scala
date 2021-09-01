d23c3a3b1d41863c99db6da059efe3c61a035514
Scala Steward <me@scala-steward.org>
2021-09-01 17:30:49 +0200

    Update scala3-library, ... to 3.0.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
 
     // Exported
     val scala2          = "2.13.6"
-    val scala3          = "3.0.1"
+    val scala3          = "3.0.2"
     val scalaJsDom      = "1.1.0"
     val scalaJsReact    = "2.0.0-RC2"
     val scalatags       = "0.9.4"
