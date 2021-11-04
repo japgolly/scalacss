256cdd4f0df537f7005c1cacb32011e1fe02d4fc
David Barri <japgolly@gmail.com>
2021-11-05 09:23:16 +1100

    Upgrade deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,14 +12,14 @@ object Dependencies {
     val scala2          = "2.13.6"
     val scala3          = "3.0.1"
     val scalaJsDom      = "2.0.0"
-    val scalaJsReact    = "2.0.0-RC4"
+    val scalaJsReact    = "2.0.0"
     val scalatags       = "0.10.0"
     val univEq          = "2.0.0"
 
     // Internal
     val cats            = "2.6.1"
     val microlibs       = "4.0.0"
-    val nyaya           = "0.11.0"
+    val nyaya           = "1.0.0"
     val reactJs         = "17.0.2"
     val utest           = "0.7.10"
   }
